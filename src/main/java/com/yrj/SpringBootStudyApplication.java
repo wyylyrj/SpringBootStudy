58e9a3ecd534c68c604752a14b749d286ca85f54
wyylyrj <461318558@qq.com>
2018-12-04 10:43:11 +0800

    SpringBootStudy

--- src/main/java/com/yrj/SpringBootStudyApplication.java
+++ src/main/java/com/yrj/SpringBootStudyApplication.java
@@ -1,12 +1,41 @@
 package com.yrj;
 
+import com.yrj.filter.SecondFilter;
+import com.yrj.listener.SecondListener;
+import com.yrj.servlet.SecondServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServletRegistrationBean(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new SecondServlet());
+        bean.addUrlMappings("/second");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean bean = new FilterRegistrationBean(new SecondFilter());
+        bean.addUrlPatterns("/second");
+        return bean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<SecondListener> getServletListenerRegistrationBean(){
+        ServletListenerRegistrationBean bean = new ServletListenerRegistrationBean(new SecondListener());
+        return bean;
     }
 }
